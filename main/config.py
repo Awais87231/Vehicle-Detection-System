834cc70c26cf3b38c59ebcd23ca53bf646771d68
ashharamir <127431173+ashharamir@users.noreply.github.com>
2023-05-14 16:40:46 +0500

    adding the config.py

---
+++ main/config.py
@@ -0,0 +1,24 @@
+#validation fields
+min_Width = 111
+min_Height = 111
+
+#video source
+vid_source = "sourcevid.mp4"
+
+pixelOffsetMargin = 7 #offset margin to eliminate possible errors
+linePosY = 650 #y coordinate of the line on the frame where we count vehicles
+
+exitKey = 27 #key to exit the program
+
+outputPath = 'C:/Users/Ashar/Documents/NUCES/Study/SEM-2/ISE/PROJECT/Newfolder/textoutputs'
+data_displayPath = "C:/Users/Ashar/Documents/NUCES/Study/SEM-2/ISE/PROJECT/Newfolder/dataDisplay.html"
+
+
+#color codes for the RECTANGLES
+rect_r = 0
+rect_g = 255
+rect_b = 0
+
+#thickness of the RECTANGLES
+rect_t = 2
+
