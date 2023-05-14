c24c5e2f72359749072aad740292bf31df2e7ca7
ashharamir <127431173+ashharamir@users.noreply.github.com>
2023-05-14 14:53:19 +0500

    Delete main directory

--- main/Validation.py
+++
@@ -1,7 +0,0 @@
-#validation fields
-min_Width = 111
-min_Height = 111
-
-#validating if its a vehicle to minimise errors
-        if not ((w >= min_Width) and (h >= min_Height)):
-            continue
